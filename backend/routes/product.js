e23cf63d75bca9614990b8927bd067a42f5f8348
sagy <sagix7004@gmail.com>
2021-12-31 11:06:26 +0200

    product update function

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {create, productById, read, remove} = require('../controllers/product');
+const {create, productById, read, remove, update} = require('../controllers/product');
 const {requireSignin, isAuth, isAdmin} = require("../controllers/auth");
 const {userById} = require('../controllers/user');
 
 router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAdmin, isAuth, create);
 router.delete('/product/:productId/:userId',requireSignin, isAdmin, isAuth, remove);
-
+router.put('/product/:productId/:userId',requireSignin, isAdmin, isAuth, update);
 
 router.param("userId", userById);
 router.param("productId", productById);
