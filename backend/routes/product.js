97b75c803c4ff9c422c9346b9082b911b593dfe8
sagy <sagix7004@gmail.com>
2022-01-02 15:48:15 +0200

    added search product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const {create, productById, read, remove, update, list, listRelated, listCategories} = require('../controllers/product');
+const {
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch
+} = require('../controllers/product');
 const {requireSignin, isAuth, isAdmin} = require("../controllers/auth");
 const {userById} = require('../controllers/user');
 
 router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAdmin, isAuth, create);
-router.delete('/product/:productId/:userId',requireSignin, isAdmin, isAuth, remove);
-router.put('/product/:productId/:userId',requireSignin, isAdmin, isAuth, update);
-
+router.delete('/product/:productId/:userId', requireSignin, isAdmin, isAuth, remove);
+router.put('/product/:productId/:userId', requireSignin, isAdmin, isAuth, update);
 router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 router.get('/products/categories', listCategories);
+router.post("/products/by/search", listBySearch);
 
 router.param("userId", userById);
 router.param("productId", productById);
