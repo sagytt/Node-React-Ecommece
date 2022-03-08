18a2ca2f5adcff49c7c118a6debfa1c1b20d76dd
sagy <sagix7004@gmail.com>
2022-03-08 13:35:45 +0200

    added .env file

--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Layout from '../core/Layout'
+import {API} from "../config";
+
 const Signup = () => (
     <Layout title="Signup" description="Just signup page">
-
+        {API}
     </Layout>
 );
 
