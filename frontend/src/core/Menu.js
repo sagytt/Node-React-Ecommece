b5b4f86dd2dba8eaf8fd84fef9f4109d256d97e7
sagy <sagix7004@gmail.com>
2022-03-09 17:25:28 +0200

    added isAuthenticated method

--- frontend/src/core/Menu.js
+++ frontend/src/core/Menu.js
@@ -1,6 +1,6 @@
-import Routes from "../Routes";
+import React, {Fragment} from "react";
 import {Link, withRouter} from 'react-router-dom';
-import {signout} from "../auth";
+import {signout, isAuthenticated} from "../auth";
 
 const isActive = (history, path) =>{
     if(history.location.pathname === path){
@@ -16,17 +16,26 @@ const Menu = ({history}) =>(
             <li className="nav-item">
                 <Link className="nav-link" style={isActive(history, '/')} to="/">Home</Link>
             </li>
-            <li className="nav-item">
-                <Link className="nav-link" style={isActive(history, '/signin')}  to="/signin">Signin</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
-            </li>
-            <li className="nav-item">
+
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, '/signin')}  to="/signin">Signin</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
+                    </li>
+                </Fragment>
+            )}
+
+
+            {isAuthenticated() && (
+                <li className="nav-item">
                 <span className="nav-link" style={{cursor: 'pointer', color: '#ffffff'}} onClick={() => signout(() =>{
                     history.push('/')
                 })}>Signout</span>
-            </li>
+                </li>
+            )}
         </ul>
     </div>
 );
